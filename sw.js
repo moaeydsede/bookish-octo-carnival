d60cad228d40da2e8893c011127153b3635ea6d2
moaeydsede <moaeydsede98@gmail.com>
2026-02-25 17:55:49 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE="ordered-pro-final";
-const ASSETS=["./","./index.html","./admin.html","./sales.html","./warehouse.html","./order.html","./styles.css","./firebase.js","./app.js","./guard.js","./utils.js","./admin.js","./sales.js","./warehouse.js","./order.js","./manifest.json"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null))));});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
